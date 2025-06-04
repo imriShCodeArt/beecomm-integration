f768217817e8c71900d93fa3a9877048897cb9b8
imriSh <59433290+imriShCodeArt@users.noreply.github.com>
2025-06-04 15:52:05 +0300

    refactor: move cron callback logic to cron/update-order-status.php

---
+++ lib/cron/update-order-status.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Main cron job callback to update order statuses from Beecomm.
+ */
+
+function beecomm_cron_update_order_status() {
+	try {
+		$orders = get_orders_by_status();
+
+		if ( empty( $orders ) ) {
+			return;
+		}
+
+		foreach ( $orders as $order ) {
+			$status_code = get_beecomm_order_status( $order->get_id() );
+
+			if ( array_key_exists( $status_code, BEECOM_ORDER_STATUS_CODE ) ) {
+				$new_status = BEECOM_ORDER_STATUS_CODE[ $status_code ];
+
+				$order->update_status( $new_status );
+
+				$is_sent = send_order_status_sms( $order->get_id(), $new_status );
+
+				if ( $is_sent ) {
+					delete_post_meta( $order->get_id(), BEECOM_ORDER_STATUS_RETRY_COUNT );
+				}
+			}
+		}
+	} catch ( Exception $e ) {
+		wofErrorLog( $e->getMessage() );
+	}
+}
