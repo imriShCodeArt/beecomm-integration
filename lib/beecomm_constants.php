09bba92188cd2fd40b99941c6a3a6945985ef3f4
imriSh <59433290+imriShCodeArt@users.noreply.github.com>
2025-06-04 16:34:43 +0300

    chore: document and clean up beecomm_constants.php
    Refactored and documented beecomm_constants.php:
    - Grouped constants by purpose (cron, API, templates, meta, etc.)
    - Added section-level PHPDoc comments for clarity and IDE support
    - Fixed incorrect newline characters in BEECOM_DEFAULT_ORDER_TEMPLATE
    - Improved readability and maintainability of tag list in BEECOMM_ORDER_GETTER_TAGS

--- lib/beecomm_constants.php
+++ lib/beecomm_constants.php
@@ -1,89 +1,78 @@
 <?php
-//setting constants
-const BEECOMM_INTEGRATION_OPTIONS        = 'beecomm_integration_options';
-const BEECOMM_ORDER_STATUS_CRON          = 'beecomm_order_status_cron';
+
+/**
+ * === General Options ===
+ */
+const BEECOMM_INTEGRATION_OPTIONS = 'beecomm_integration_options';
+const BEECOMM_NUMBER_OF_ORDER_TO_PROCESS = 'beecomm_number_of_order_to_process';
+const BEECOMM_NUMBER_OF_ORDER_TO_PROCESS_DEFAULT = 30;
+const ORDER_METHOD = 'order_method';
+const BEECOM_ORDER_PREPARATION_TIME = 'preparation_time';
+
+/**
+ * === Cron Settings ===
+ */
+const BEECOMM_ORDER_STATUS_CRON = 'beecomm_order_status_cron';
 const BEECOMM_ORDER_STATUS_CRON_INTERVAL = 'beecomm_order_status_cron_interval';
-const BEECOMM_ADMIN_PHONE                = 'beecomm_admin_phone';
-const ORDER_METHOD                       = 'order_method';
-const BEECOM_ORDER_PREPARATION_TIME      = 'preparation_time';
 
+/**
+ * === Admin Settings ===
+ */
+const BEECOMM_ADMIN_PHONE = 'beecomm_admin_phone';
 
-//delivery template
-const BEECOMM_ORDER_HOLD_TEMPLATE     = 'beecomm_order_hold_template';
+/**
+ * === SMS Templates (Delivery & Pickup) ===
+ */
+const BEECOMM_ORDER_HOLD_TEMPLATE = 'beecomm_order_hold_template';
 const BEECOMM_ORDER_COMPLETE_TEMPLATE = 'beecomm_order_complete_template';
-//pickup template
-const BEECOMM_ORDER_HOLD_TEMPLATE_PICKUP     = 'beecomm_order_hold_template_pickup';
+const BEECOMM_ORDER_HOLD_TEMPLATE_PICKUP = 'beecomm_order_hold_template_pickup';
 const BEECOMM_ORDER_COMPLETE_TEMPLATE_PICKUP = 'beecomm_order_complete_template_pickup';
 
-const BEECOMM_NUMBER_OF_ORDER_TO_PROCESS = 'beecomm_number_of_order_to_process';
-
-
-//meta constants
+/**
+ * === Meta Keys ===
+ */
 const BEECOM_ORDER_STATUS_POST_META_KEY = '_beecomm_order_status';
-const BEECOM_ORDER_STATUS_RETRY_COUNT   = '_beecomm_order_status_retry_count';
+const BEECOM_ORDER_STATUS_RETRY_COUNT = '_beecomm_order_status_retry_count';
 
-//api constants
+/**
+ * === API Constants ===
+ */
 const BEECOM_ORDER_STATUS_API_URL = '/api/v2/services/orderCenter/getOrderStatus';
 
-//order status constants
+/**
+ * === Order Status Mapping ===
+ * Used to map numeric Beecomm responses to WooCommerce statuses.
+ */
 const BEECOM_ORDER_STATUS_CODE = [
-	0 => 'wc-on-hold',
-	1 => 'wc-completed',
-	2 => 'wc-pending',
+    0 => 'wc-on-hold',
+    1 => 'wc-completed',
+    2 => 'wc-pending',
 ];
 
-//default order status template
-const BEECOMM_NUMBER_OF_ORDER_TO_PROCESS_DEFAULT = 30;
-const BEECOMM_ORDER_GETTER_TAGS                  = 'Order Message Template.<br>
-            Use the following tags to customize the message.<br>
-            tags will call the getters of the order object, so you can use any getter of the order object.:<br>
-            {{id}} - The order ID<br>
-            {{status}} - The order status<br>
-            {{total}} - The order total<br>
-            {{formatted_billing_address}} - Get the Formatted Billing Address<br>
-            {{preparation_time}} - Get Order Preparation time<br>
-            {{billing_first_name}} - Get the Billing First Name<br>
-            {{billing_last_name}} - Get the Billing Last Name<br>
-            {{billing_company}} - Get the Billing Company<br>
-            {{billing_address_1}} - Get the Billing Address 1<br>
-            {{billing_address_2}} - Get the Billing Address 2<br>
-            {{billing_city}} - Get the Billing City<br>
-            {{billing_state}} - Get the Billing State<br>
-            {{billing_postcode}} - Get the Billing Postcode<br>
-            {{billing_country}} - Get the Billing Country<br>
-            {{billing_email}} - Get the Billing Email<br>
-            {{billing_phone}} - Get the Billing Phone<br>
-            {{formatted_shipping_address}} - Get the Formatted Shipping Address<br>
-            {{shipping_first_name}} - Get the Shipping First Name<br>
-            {{shipping_last_name}} - Get the Shipping Last Name<br>
-            {{shipping_company}} - Get the Shipping Company<br>
-            {{shipping_address_1}} - Get the Shipping Address 1<br>
-            {{shipping_address_2}} - Get the Shipping Address 2<br>
-            {{shipping_city}} - Get the Shipping City<br>
-            {{shipping_state}} - Get the Shipping State<br>
-            {{shipping_postcode}} - Get the Shipping Postcode<br>
-            {{shipping_country}} - Get the Shipping Country<br>
-            {{shipping_phone}} - Get the Shipping Phone<br>
-            {{order_items}} - Get the Order Items<br>
-            {{order_notes}} - Get the Order Notes<br>
-            {{order_date}} - Get the Order Date<br>
-            {{order_payment_method}} - Get the Order Payment Method<br>
-            {{order_payment_method_title}} - Get the Order Payment Method Title<br>
-            {{order_shipping_method}} - Get the Order Shipping Method<br>
-            {{order_shipping_method_title}} - Get the Order Shipping Method Title<br>
-            {{order_tax}} - Get the Order Tax<br>
-            {{order_shipping}} - Get the Order Shipping<br>
-            {{order_discount}} - Get the Order Discount<br>
-            {{order_total}} - Get the Order Total<br>
-            {{order_version}} - Get the Order Version<br>
-            {{order_status}} - Get the Order Status<br>
-            {{order_currency}} - Get the Order Currency<br>
-            {{order_customer_ip}} - Get the Order Customer IP<br>
-            ';
-const BEECOM_DEFAULT_ORDER_TEMPLATE              = '
-	Order Status Updated./n
-	Dear {{billing_first_name}}/n
-	Your order {{id}} Status has been Updated to {{status}}./n
-	Preparation Time: {{preparation_time}}/n
-	For more details please visit your account./n
-	Thank you for shopping with us.';
+/**
+ * === Template Description Tags ===
+ * These can be used in SMS message templates.
+ */
+const BEECOMM_ORDER_GETTER_TAGS = 'Order Message Template.<br>
+Use the following tags to customize the message.<br>
+Tags will call the getters of the order object, so you can use any getter of the order object:<br>
+{{id}}, {{status}}, {{total}}, {{formatted_billing_address}}, {{preparation_time}},<br>
+{{billing_first_name}}, {{billing_last_name}}, {{billing_company}}, {{billing_address_1}}, {{billing_address_2}},<br>
+{{billing_city}}, {{billing_state}}, {{billing_postcode}}, {{billing_country}}, {{billing_email}}, {{billing_phone}},<br>
+{{formatted_shipping_address}}, {{shipping_first_name}}, {{shipping_last_name}}, {{shipping_company}},<br>
+{{shipping_address_1}}, {{shipping_address_2}}, {{shipping_city}}, {{shipping_state}}, {{shipping_postcode}},<br>
+{{shipping_country}}, {{shipping_phone}}, {{order_items}}, {{order_notes}}, {{order_date}},<br>
+{{order_payment_method}}, {{order_payment_method_title}}, {{order_shipping_method}},<br>
+{{order_shipping_method_title}}, {{order_tax}}, {{order_shipping}}, {{order_discount}},<br>
+{{order_total}}, {{order_version}}, {{order_status}}, {{order_currency}}, {{order_customer_ip}}';
+
+/**
+ * === Default Message Template ===
+ */
+const BEECOM_DEFAULT_ORDER_TEMPLATE = '
+Order Status Updated.
+Dear {{billing_first_name}},
+Your order {{id}} status has been updated to {{status}}.
+Preparation Time: {{preparation_time}}
+For more details, please visit your account.
+Thank you for shopping with us.';
